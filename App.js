7da438d5074d334f87ca4647e6505bc72ae412fd
Erick Ferreira <erickferreiradev@gmail.com>
2024-08-14 23:57:52 -0300

    Operações inseridas na calculadora

--- App.js
+++ App.js
@@ -1,35 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
 
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 import GlobalStyle from './GlobalStyle';
 
-export default function App() {
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0,0],
+  current: 0,
+}
+
+export default class App extends Component {
   state = {
-    displayValue: '0'
+    ...initialState
   }
 
   addDigit = n => {
-    this.setState({ displayValue: n })
+    if ( n === '.' && this.state.displayValue.includes('.')){
+      return
+    }  
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({displayValue, clearDisplay: false})
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: '0'})
+    this.setState({...initialState})
   }
 
 
   setOperation = operation => {
-    
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true})
+    } else {
+      const equals = opertation === '='
+      const values = {...this.state.values}
+    } try {
+      values[0] = eval(`${values[0]} ${operation} ${values[1]}`)
+    } catch (e) {
+      values[0] = this.state.values[0]
+    }
+
+    values[1] = 0
+    this.setState({
+      displayValue: values[0],
+      operation: equal ? null : operation,
+      current: equals ? 0 : 1,
+      clearDisplay: !equals,
+      values,
+    })
   }
 
+  render() {
   return (
     <View style={[GlobalStyle.safeArea, styles.container]}>
       <Display value={this.state.displayValue}/>
       <View style={styles.button}>
         <Button label='AC' triple onClick={this.clearMemory}/>
         <Button label='/' operation onClick={() => this.setOperation('/')}/>
-        <Button label='7' onClick={() => this.addDigit(7)}/>
+        <Button label='7' onClick={this.addDigit}/>
         <Button label='8' onClick={() => this.addDigit(8)}/>
         <Button label='9' onClick={() => this.addDigit(9)}/>
         <Button label='*' operation onClick={() => this.setOperation('*')}/>
@@ -48,7 +90,7 @@ export default function App() {
     </View>
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
